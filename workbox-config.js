225b4bc1315332737db5fc2df714457f4cdfdcb0
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-11-10 15:01:03 +0700

    Committing work in progress

---
+++ workbox-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  globDirectory: 'public/',
+  globPatterns: ['**/*.{html,xml,css,ico,png,json,svg,jpg,js,md,txt}'],
+  swDest: 'public/sw.js',
+  swSrc: 'workbox-custom-sw.js'
+};
