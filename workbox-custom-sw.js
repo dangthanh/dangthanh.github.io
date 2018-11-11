eca5b9b3b0ea7d240c5e713df5bc3d3e90a10cfa
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-11-11 10:49:30 +0700

    Committing work in progress

--- workbox-custom-sw.js
+++ workbox-custom-sw.js
@@ -2,4 +2,23 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js'
 );
 
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets'
+  })
+);
+
+workbox.routing.registerRoute(
+  'https//codepen.io/assets/embed/ei.js',
+  workbox.strategies.cacheFirst()
+);
+
+workbox.routing.registerRoute(
+  'https//unpkg.com/vue@2.5.17/dist/vue.min.js',
+  workbox.strategies.cacheFirst()
+);
+
+workbox.strategies.networkFirst();
+workbox.googleAnalytics.initialize();
 workbox.precaching.precacheAndRoute([]);
