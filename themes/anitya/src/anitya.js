6c3efe4ee8125970ade1807ef963e286b36282db
Dang Van Thanh <dangvanthanh@dangthanh.org>
2019-03-24 01:19:35 +0700

    Committing work in progress

--- themes/anitya/src/anitya.js
+++ themes/anitya/src/anitya.js
@@ -1,3 +1,4 @@
+import { GuessPlugin } from 'guess-webpack';
 import { getLinks, getCanvas, getLazyImage } from './utils';
 import Appearance from './components/Appearance';
 import Caniuse from './components/Caniuse';
@@ -45,6 +46,8 @@ document.addEventListener('DOMContentLoaded', function() {
       'c-caniuse': Caniuse
     }
   });
+
+  new GuessPlugin({ GA: 'UA-41117458-1' });
 
   getLinks();
   getCanvas();
