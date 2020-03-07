3a6e015d7b0d399da87b37bcc8ce9365866c5e15
Dang Van Thanh <dangvanthanh@dangthanh.org>
2020-03-07 18:28:41 +0700

    Committing work in progress

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="max-w-2xl mx-auto px-5">
-    <article>
-      <header>
-        <h1
-          class="text-4xl xl:text-5xl mb-3 font-semibold font-merriweather"
-        >{{ post.attributes.title }}</h1>
-        <div class="mb-4">
-          <span
-            v-for="tag in post.attributes.tags"
-            :key="tag"
-            class="inline-block mr-2 text-gray-500 bg-gray-200 p-2 rounded uppercase text-xs leading-none"
-          >{{ tag }}</span>
-        </div>
-        <time
-          class="text-gray-700 text-sm block"
-        >{{ $dateFns.format(post.attributes.date, 'MMMM dd, yyyy') }}</time>
-      </header>
+  <article class="article-single">
+    <header>
+      <h1
+        class="text-4xl xl:text-5xl mb-3 font-semibold font-merriweather"
+      >{{ post.attributes.title }}</h1>
+      <div class="mb-4">
+        <span
+          v-for="tag in post.attributes.tags"
+          :key="tag"
+          class="inline-block mr-2 text-gray-500 bg-gray-200 p-2 rounded uppercase text-xs leading-none"
+        >{{ tag }}</span>
+      </div>
+      <time
+        class="text-gray-700 text-sm block"
+      >{{ $dateFns.format(post.attributes.date, 'MMMM dd, yyyy') }}</time>
+    </header>
+    <div class="max-w-2xl mx-auto px-5">
       <hr class="border-b mt-6 mb-3 w-48 inline-block border-gray-300 border-solid" />
-      <div v-html="post.html"></div>
-    </article>
-  </div>
+    </div>
+    <div v-html="post.html"></div>
+  </article>
 </template>
 
 <script>
