675669290e9c0286fb97b6f60766a6ae16d12ecf
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-03-03 09:49:39 +0700

    feat: update tag and optimize images

--- _astro/hoisted.Uqb-OILy.js
+++
@@ -1,1 +0,0 @@
-import{A as r}from"./atropos.H_kL7MPM.js";import"./hoisted.iyld1NVO.js";const e=document.querySelectorAll(".atropos-experience");[...e].forEach(o=>{r({el:o,shadowScale:.25})});
--- _astro/hoisted.uzqkkayr.js
+++
@@ -1,1 +0,0 @@
-import{A as r}from"./atropos.H_kL7MPM.js";import"./hoisted.iyld1NVO.js";const t=document.querySelectorAll(".atropos-github-card");[...t].forEach(o=>{r({el:o,shadowScale:.625})});
